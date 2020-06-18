718eeae735a912de993f5703ab55012793dfbdac
Phil Chodrow <pchodrow@gmail.com>
2020-06-17 21:18:28 -0400

    minor tweaks to cut stuff for first_term_eval performance
    May not matter if we move to first_term_v2 as the standard evaluation method.

--- jl/cut.jl
+++ jl/cut.jl
@@ -19,18 +19,15 @@ function first_term_eval(H::hypergraph,c::Array{Int64,1}, Ω)
     obj = 0
     for l = kmin:kmax
         El = H.E[l]
-        
         for edge in keys(El)
-    
-            clus_e = c[edge]    # set of clusters
-            weight = El[edge]
-            obj   += weight*log(Ω(clus_e; mode="group"))
+            p = partitionize(c[edge])
+            obj += El[edge]*log(Ω(p; mode="partition", k = l))
         end
     end
     return obj
 end
 
-function first_term_v2(H::Vector{Vector{Int64}},w::Array{Float64,1},c::Array{Int64,1}, Ω)
+function first_term_v2(H::Vector{Vector{Int64}},w::Array{Int64,1},c::Array{Int64,1}, Ω)
     """
     Second version: store penalties first,
     and more importantly, a faster way to compute Ω(z_e)
@@ -38,14 +35,11 @@ function first_term_v2(H::Vector{Vector{Int64}},w::Array{Float64,1},c::Array{Int
     c: array storing cluster indices; c[i] is the cluster node i is in
     kmax: maximum hyperedges size in H
     Ω: Interaction function. Must have been built with option by_size = true using buildΩ().  
-    e.g., given hyperedge size function fk and partition function fp, can use:
-        ff = p->fp(p)*fk(sum(p))
-        Om = build_omega(kmax,ff) # kmax = max hyperedge size
     """
     obj = 0
 
     for i = 1:length(w)
-
+        
         edge = H[i]
         l = length(edge)
         clus_e = c[edge]    # set of clusters
@@ -61,11 +55,13 @@ end
 
 """
 Convert a hypergraph from old to new format.
+PC: I find that first_term_v2 is around twice as fast as first_term_eval -- if that turns out to make enough of a difference, then maybe this should actually be our core data structure? 
+PC: If I understand correctly, this is advantageous to do because indexing Arrays is faster than Dict lookups?
 """
 function hyperedge_formatting(H::hypergraph)
 
     Hyperedges = Vector{Vector{Int64}}()
-    weights = Vector{Float64}()
+    weights = Vector{Int64}()
     for key in keys(H.E)
         ed = collect(keys(H.E[key]))
         wt = collect(values(H.E[key]))
