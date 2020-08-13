7c27c26fff2eafa20ce020e3e35e13ae5ef79e35
nveldt <nateveldt@gmail.com>
2020-08-13 15:56:11 -0400

    binary flag on clique expansion

--- src/warmstart.jl
+++ src/warmstart.jl
@@ -2,7 +2,7 @@
 include("graph_louvain.jl")
 include("HSBM.jl")
 
-function CliqueExpansion(H::hypergraph,weighted::Bool=true)
+function CliqueExpansion(H::hypergraph,weighted::Bool=true,binary::Bool=false)
     """
     Weighted clique expansion where a hyperedge e is expanded to a
     weighted clique with each edge having weight 1/(|e| - 1)
@@ -34,8 +34,11 @@ function CliqueExpansion(H::hypergraph,weighted::Bool=true)
     for i = 1:n; A[i,i] = 0.0; end
     SparseArrays.dropzeros!(A)
     A = SparseArrays.sparse(A+A')
+    if binary
+        I,J,V = findnz(A)
+        A = sparse(I,J,ones(length(I)),n,n)
+    end
     return A
-
 end
 
 function CliqueExpansionModularity(H::hypergraph,gamma::Float64=1.0,weighted::Bool=true,randflag::Bool=false)
@@ -58,8 +61,8 @@ function VanillaModularity(A::SparseArrays.SparseMatrixCSC{Float64,Int64},gamma:
 
     Code: https://github.com/nveldt/ParamCC/blob/master/src/Graph_Louvain.jl
     """
-    
-    
+
+
     d = vec(sum(A,dims = 2))
     n = length(d)
     vol = sum(d)
@@ -78,7 +81,7 @@ function computeDyadicResolutionParameter(H, Z)
     """
     compute the dyadic resolution parameter associated to a partition using the formula from Newman (2016): https://arxiv.org/abs/1606.02319
     """
-    
+
     G = CliqueExpansion(H)
     I, J = SparseArrays.findnz(G)
     n = maximum(I) # number of nodes
@@ -93,18 +96,18 @@ function computeDyadicResolutionParameter(H, Z)
     for k in 1:length(I)
         D[I[k]] += G[I[k], J[k]]
         if Z[I[k]] == Z[J[k]]
-            m_in  += G[I[k], J[k]]/2 
+            m_in  += G[I[k], J[k]]/2
         else
-            m_out += G[I[k], J[k]]/2 
+            m_out += G[I[k], J[k]]/2
         end
     end
-    
+
     # compute resolution parameter
     V = [sum(D[Z.==k]) for k in 1:maximum(Z)]
     ω_in = 4*m*m_in / (sum(V.^2))
     ω_out = (2m - 2m_in)/(2m - (sum(V.^2)/(2m)))
     γ = (ω_in - ω_out)/(log(ω_in) - log(ω_out))
-        
+
     return(γ)
 end
 
@@ -113,15 +116,15 @@ function dyadicModularity(H, Z, γ)
     I, J = SparseArrays.findnz(G)
     n = maximum(I) # number of nodes
     m = sum(G)/2     # number of edges
-    
+
     D = zero(1.0.*collect(1:n))
 
     for k in 1:length(I)
         D[I[k]] += G[I[k], J[k]]
     end
-    
+
     Q = 0.0
-    
+
     for i = 1:n, j = 1:n
         if Z[i] == Z[j]
             Q += G[i, j] - γ*(D[i]*D[j])/(2m)
