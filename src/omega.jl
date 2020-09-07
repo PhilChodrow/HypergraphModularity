a5799cde4c657c00e19286d2c83b30783d91b134
Phil Chodrow <pchodrow@gmail.com>
2020-09-07 09:24:24 -0700

    update omega.jl

--- src/omega.jl
+++ src/omega.jl
@@ -42,36 +42,3 @@ function sumOfExteriorDegreesIntensityFunction(ω, kmax)
     aggregator = p->(length(p), sum(p))
     return IntensityFunction(ω, P, range, aggregator)
 end
-
-
-
-
-
-
-
-# function buildΩ(f, α0, kmax)
-#     """
-#     f : function, takes in *partitions* and returns real numbers. May need to check on whether Float64 is big enough
-#     α0: parameters passed to f
-#     kmax: the size of the largest collection of nodes for which Ω should be able to compute
-#     """
-#     # memoization
-#     ᾱ = α0
-#     Om = Dict(k => Dict(p => f(p,ᾱ) for p in Combinatorics.partitions(k)) for k = 1:kmax)
-#     function Ω(x; α, mode="group")
-#
-#         # check if the parameter has been updated and re-memoize if so
-#         if !(α ≈ ᾱ)
-#             ᾱ = α
-#             Om = Dict(k => Dict(p => f(p,ᾱ) for p in Combinatorics.partitions(k)) for k = 1:kmax)
-#         end
-#
-#         # otherwise, return required value from Om
-#         if mode == "group"
-#             return Om[length(x)][partitionize(x)]
-#         elseif mode == "partition"
-#             return Om[sum(x)][x]
-#         end
-#     end
-#     return Ω
-# end
