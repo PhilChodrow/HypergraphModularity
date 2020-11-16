3059f0a3ce519aa78a98cf52a3f605359486351c
Austin Benson <arbenson@gmail.com>
2020-11-16 12:46:33 -0500

    add option for minimum edge size

--- src/read_data.jl
+++ src/read_data.jl
@@ -18,26 +18,27 @@ function read_hypergraph_labels(dataname::String)
     return labels
 end
 
-function read_hypergraph_edges(dataname::String, maxsize::Int64=25)
+function read_hypergraph_edges(dataname::String, maxsize::Int64=25, minsize::Int64=2)
     E = Dict{Integer, Dict}()
     open("data/$dataname/hyperedges-$dataname.txt") do f
         for line in eachline(f)
             edge = [parse(Int64, v) for v in split(line, ',')]
             sort!(edge)
-            if length(edge) > maxsize; continue; end
-            sz = length(edge)
-            if !haskey(E, sz)
-                E[sz] = Dict{}()
+            if minsize <= length(edge) <= maxsize
+                sz = length(edge)
+                if !haskey(E, sz)
+                    E[sz] = Dict{}()
+                end
+                E[sz][edge] = 1
             end
-            E[sz][edge] = 1
         end
     end
     return E
 end
 
-function read_hypergraph_data(dataname::String, maxsize::Int64=25)
+function read_hypergraph_data(dataname::String, maxsize::Int64=25, minsize::Int64=2)
     labels = read_hypergraph_labels(dataname)
-    E = read_hypergraph_edges(dataname, maxsize)
+    E = read_hypergraph_edges(dataname, maxsize, minsize)
     
     n = length(labels)
     D = zeros(Int64, n)
