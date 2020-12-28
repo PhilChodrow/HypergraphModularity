afedea4edcecc065c6d40553b0bc6952f56c378b
nveldt <nateveldt@gmail.com>
2020-12-28 12:22:12 -0500

    synthetic hypergraph louvain performance experiments

--- src/synthetic_hypergraphs.jl
+++ src/synthetic_hypergraphs.jl
@@ -36,7 +36,6 @@ function GenerateHypergraphAll(n,m,K,pvals,rmin,rmax,cluster_sizes=ones(K),r_siz
         push!(Clusters[c],i)
     end
 
-
     U = Vector{Int64}()
     E = Vector{Int64}()
     EdgeList = Vector{Vector{Int64}}()
@@ -191,7 +190,8 @@ function learn_alpha_wrapper(H,c,kmax,n)
     end
     Ω = allOrNothingIntensityFunction(ω, kmax)
     st = time()
-    α = learnParameters(H, c, Ω, α; n_iters = 10, amin = -10, amax = 10)
+    α = learnParameters(H, c, Ω, α; max_iters = 20, verbose = false, tol = 1e-2)#; n_iters = 10, amin = -10, amax = 10)
+    # Ω = estimateΩEmpirically(H, c)
     learntime = time()-st
     cut_weights = zeros(kmax)
     vol_weights = zeros(kmax)
@@ -199,5 +199,58 @@ function learn_alpha_wrapper(H,c,kmax,n)
         cut_weights[k] = log(ω([1,k],α))-log(ω([0,k],α))
         vol_weights[k] = ω([1,k],α)-ω([0,k],α)
     end
-    cut_weights, vol_weights, learntime, α
+    return cut_weights, vol_weights, learntime, α
+end
+
+function learn_omega_aon(e2n,Z,kmax,d,n)
+    # assumes kmin is 2
+    # assumes hypergraph is unweighted
+    # e2n is the edge-to-node list
+    L = maximum(Z)
+    m = length(e2n)
+    ClusVol = zeros(L)			# volume of clusters
+    for i = 1:n
+        ClusVol[Z[i]] += d[i]
+    end
+
+    # initialize to one as a heuristic to avoid taking log of 0
+    EdgesAndCuts = zeros(2,kmax)
+    EdgesAndCuts[1,:] = ones(1,kmax)
+
+    # Compute the cut penalty for each hyperedge size
+    for j = 1:m
+        edge = e2n[j]
+        k = length(edge)
+        EdgesAndCuts[1,k] += 1      # just counting number of hyperedges
+        iscut = notsame(edge,Z,Z[edge[1]])
+        if iscut
+            EdgesAndCuts[2,k] += 1
+        end
+    end
+
+    # top is in probability, bottom is out probability
+    omega = zeros(2,kmax)
+    volV = sum(ClusVol)
+
+    for k = 2:kmax
+
+        # get the volume piece
+        volsums = 0
+        for l = 1:L
+            volsums += ClusVol[l]^k
+        end
+        volVk = volV^k
+
+        omega[1,k] = (EdgesAndCuts[1,k] - EdgesAndCuts[2,k])/volsums
+        omega[2,k] = EdgesAndCuts[2,k]/(volVk - volsums)
+    end
+
+    cut_weights = zeros(kmax)
+    vol_weights = zeros(kmax)
+    for k = 2:kmax
+        cut_weights[k] = log(omega[1,k])-log(omega[2,k])
+        vol_weights[k] = omega[1,k]-omega[2,k]
+    end
+    return cut_weights, vol_weights,omega, EdgesAndCuts
+
 end
